83d4301e287aba9dfbf7c6b787021b0c555fa4d7
Pawel-Serkowski <paw.serkowski@gmail.com>
2024-04-04 14:30:37 +0200

    Added icon and fixed navbar sizin

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import "./button.scss";
 const Button = ({ style, text, id, link = 0 }) => {
     return (
-        <a className={`${style}`} href={link ? id : `#${id}`} target={link ? "_blank" : "_self"}>
+        <a className={`${style}`} href={link ? id : `/#${id}`} target={link ? "_blank" : "_self"}>
             {text}
         </a>
     );
